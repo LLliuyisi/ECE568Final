92867492e30ebc2af525764de30b9dc67cbc3239
Shawn Liu <liuyisi2018@outlook.com>
2020-05-07 23:40:19 -0400

    For REST API

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
@@ -14,3 +16,4 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models
+
